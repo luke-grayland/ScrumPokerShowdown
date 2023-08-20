76f2ef6e4db10ec0024a3e2826c918a1fc9936ff
Luke Grayland <lgrayland96@gmail.com>
2023-08-20 17:59:42 +0100

    small ui updates

--- ScrumPoker-react/ClientApp/src/components/LoadingScreen/LoadingScreen.js
+++ ScrumPoker-react/ClientApp/src/components/LoadingScreen/LoadingScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import ImageFadeIn from "react-image-fade-in";
 
 const LoadingScreen = () => {
     
@@ -8,7 +9,10 @@ const LoadingScreen = () => {
         <>
             <div className="loadingScreen">
                 <div className="logoDiv">
-                    <img src={"/ScrumPokerLogoWithText.png"} alt="Scrum Poker Logo" id="homeScreenLogo" />
+                    <ImageFadeIn src={"/ScrumPokerLogoWithText.png"} 
+                                 opacityTransition={1} 
+                                 id="homeScreenLogo" 
+                                 alt="Scrum Poker Showdown Logo"/>
                 </div>
                 <FontAwesomeIcon icon={faSpinner} spin className="loadingSpin"/>
             </div>
