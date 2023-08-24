f070ea3fc9d53cc356c94c42212aeaa2ca4b788e
Luke Grayland <lgrayland96@gmail.com>
2023-08-24 22:38:51 +0100

    updated html tags for google seo

--- ScrumPoker-react/ClientApp/src/components/LoadingScreen/LoadingScreen.js
+++ ScrumPoker-react/ClientApp/src/components/LoadingScreen/LoadingScreen.js
@@ -4,20 +4,16 @@ import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import ImageFadeIn from "react-image-fade-in";
 
 const LoadingScreen = () => {
-    
     return (
-        <>
-            <div className="loadingScreen">
-                <div className="logoDiv">
-                    <ImageFadeIn src={"/ScrumPokerLogoWithText.png"} 
-                                 opacityTransition={1} 
-                                 id="homeScreenLogo" 
-                                 alt="Scrum Poker Showdown Logo"/>
-                </div>
-                <FontAwesomeIcon icon={faSpinner} spin className="loadingSpin"/>
+        <div className="loadingScreen">
+            <div className="logoDiv">
+                <ImageFadeIn src={"/ScrumPokerLogoWithText.png"}
+                             opacityTransition={1}
+                             id="homeScreenLogo"
+                             alt="Scrum Poker Showdown Logo"/>
             </div>
-        </>
-        
+            <FontAwesomeIcon icon={faSpinner} spin className="loadingSpin"/>
+        </div>
     );
 };
 
