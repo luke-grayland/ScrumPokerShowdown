f0e4842936c98a5fca1ffe1d53cbce80b65c236e
Luke Grayland <lgrayland96@gmail.com>
2023-09-20 11:40:28 +0100

    added player has voted tick

--- ScrumPoker-react/ClientApp/src/components/PlayerCard/PlayerCard.js
+++ ScrumPoker-react/ClientApp/src/components/PlayerCard/PlayerCard.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const PlayerCard = ({player, showScores}) => {
+    const [playerHasVoted, setPlayerHasVoted] = useState(false)
+    
+    useEffect(() => {
+        if(player.Vote)
+            setPlayerHasVoted(player.Vote > 0)
+    }, [player])
+    
+    useEffect(() => {
+        if (!showScores)
+            setPlayerHasVoted(false)
+    }, [showScores])
+    
     return(
         <div className="playerCardAndName">
-            <div className={`player scrumPokerCard card shadowSmall ${player.Vote > 0 
-                ? "playerHasVoted" : ""} ${showScores ? "" 
-                : "cardValueHidden"}`}>
+            <div className={`player scrumPokerCard card shadowSmall ${playerHasVoted 
+                ? "playerHasVoted" : ""} ${showScores ? "" : "cardValueHidden"}`}>
+                { (playerHasVoted && !showScores) &&
+                    <img src="/cardTick.png" alt="Player Has Voted Tick" className="playerHasVotedTick" />    
+                }
                 <h2>{ (showScores && player.Vote > 0) && player.Vote }</h2>
             </div>
             <h5 className="playerName">{player.Name}</h5>
